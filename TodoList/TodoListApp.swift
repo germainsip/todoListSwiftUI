dfae247d0de6c745171b82144af27f3ad4601d4b
Germain SIPIERE <gsipiere@gmail.com>
2023-08-05 18:13:47 +0200

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by GERMAIN SIPIERE on 05/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
